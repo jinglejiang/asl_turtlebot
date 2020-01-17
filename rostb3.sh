9893d064c11cb27c145aa212d24eaa3e0295da8d
jinglejiang <jxj250@case.edu>
2020-01-16 18:15:42 -0800

    startting point working

--- rostb3.sh
+++ rostb3.sh
@@ -7,5 +7,5 @@
 # to replace <turtlebot_name> and <network_interface> below, 
 # then comment this line out by preceding it with a #\n!!!!!!!!!!!!!!!!!!!!"
 export ROS_MASTER_URI=http://asteroid.local:11311
-export ROS_IP=$(ifconfig wlp61s0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
-export ROS_HOSTNAME=$(ifconfig wlp61s0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
+export ROS_IP=$(ifconfig ens33 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
+export ROS_HOSTNAME=$(ifconfig ens33 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
